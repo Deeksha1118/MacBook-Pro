d0fe933866c3d4f4384333ad3f03867149bfadf5
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-10-15 00:42:07 +0530

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { navLinks } from '../constants'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src="/logo.svg" alt="Apple logo" />
+
+            <ul>
+                {navLinks.map(({ label }) => (                  //rather than using curly braces, use parentheses to return JSX
+                    <li key = {label}>
+                        <a href="{label}">{label}</a>
+                    </li>
+                ))}
+            </ul>
+
+            <div className='flex-center gap-3'>
+                <button>
+                    <img src="/search.svg" alt="Search" />
+                </button>
+                <button>
+                    <img src="/cart.svg" alt="Cart" />
+                </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
