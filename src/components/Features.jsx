34d277c039affef4cc27943b384b4a1e04bf4323
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-10-27 23:17:16 +0530

    added performnance section

---
+++ src/components/Features.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Features = () => {
+    return (
+      <div>
+        Features Component
+      </div>
+    )
+}
+
+export default Features
---
+++ src/components/Highlights.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Highlights = () => {
+  return (
+    <div>
+      Highlights Component
+    </div>
+  )
+}
+
+export default Highlights
